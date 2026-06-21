0aa1a9fad948843fbf7c5c3bf0d39e0f180c7457
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-06-21 21:48:03 +0530

    Time: 9 ms (81.71%), Space: 83.4 MB (77.88%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,6 +1,29 @@
 class Solution {
-    public int maxIceCream(int[] cost, int coin) {
-        Arrays.sort(cost);
+    int[] countSort(int arr[]){
+        int max= arr[0];
+        for(int i=0;i<arr.length;i++){
+            max= Math.max(max,arr[i]);
+        }
+        int freq[]= new int[max+1];
+        for(int i=0;i<arr.length;i++){
+            freq[arr[i]]++;
+        }
+        for(int i=1;i<freq.length;i++){
+            freq[i]= freq[i]+freq[i-1];
+        }
+        int ans[]= new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+            int v= freq[arr[i]];
+            ans[v-1]= arr[i];
+            freq[arr[i]]--;
+        }
+        return ans;
+
+    }
+    public int maxIceCream(int[] c, int coin) {
+        // Arrays.sort(cost);
+        int cost[]= new int[c.length];
+        cost =countSort(c);
         int ans=0;
         for(int i=0;i<cost.length;i++){
             if(cost[i]<=coin){
