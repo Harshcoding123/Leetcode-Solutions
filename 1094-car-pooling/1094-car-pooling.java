b368cd8acab1d4ba90fa253b74069a596fafa28c
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-06-25 14:13:39 +0530

    Time: 1 ms (100%), Space: 46.4 MB (26.76%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,21 +1,19 @@
 class Solution {
-    public boolean carPooling(int[][] a, int cap) {
-        Arrays.sort(a,(p,b)->Integer.compare(p[1],b[1]));
-        PriorityQueue<int []>pq =new PriorityQueue<>((p,b)->(p[0]-b[0]));
-        int c =a[0][0];
-        int p= a[0][1];
-        int d =a[0][2];
-        if(c>cap) return false;
-        pq.add(new int[]{d,c});
-        for(int i=1;i<a.length;i++){
-            while(pq.size()>0&&a[i][1]>=pq.peek()[0]){
-                c-=pq.remove()[1];
-            }
-            if(c+a[i][0]>cap) return false;
-            c+=a[i][0];
-            pq.add(new int[]{a[i][2],a[i][0]});
+    public boolean carPooling(int[][] t, int cap) {
+        int a[] =new int[1001];
+        
+        for(int i=0;i<t.length;i++){
+            a[t[i][1]]+= t[i][0];
+            a[t[i][2]]-= t[i][0];
+          
+        }
+        int s=0;
+        for(int i=0;i<1001;i++){
+            s+=a[i];
+            if(s>cap) return false;
         }
         return true;
+
         
     }
 }
