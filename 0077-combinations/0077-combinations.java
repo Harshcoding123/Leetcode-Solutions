27f49cb7d877600e010499d1608caf1360228129
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-07-19 12:03:12 +0530

    Time: 14 ms (96.66%), Space: 98 MB (46.64%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,26 +1,28 @@
 class Solution {
     void func(int n,int k,int idx,ArrayList<Integer>a,List<List<Integer>>ans){
         
-        a.add(idx);
+       
         if(a.size()==k){
         
-            ans.add(new ArrayList(a));
-            a.remove(a.size()-1);
+            ans.add(new ArrayList<>(a));
+            
             return;
         }
-        for(int i=idx+1;i<=n;i++){
-            func(n,k,i,a,ans);
+        for(int i=idx;i<=n;i++){
+            a.add(i);
+            func(n,k,i+1,a,ans);
+            a.remove(a.size()-1);
             
         }
-        a.remove(a.size()-1);
+        
 
     }
     public List<List<Integer>> combine(int n, int k) {
         ArrayList<Integer>a =new ArrayList<>();
         List<List<Integer>>ans= new ArrayList<>();
-        for(int i=1;i<=n;i++){
-        func(n,k,i,a,ans);
-        }
+        
+        func(n,k,1,a,ans);
+        
         return ans;
         
     }
