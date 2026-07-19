1fe92848ac78b906f002e4bda5aba6c5a77af55d
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-07-19 11:23:23 +0530

    Time: 17 ms (47.6%), Space: 46.8 MB (23.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,24 @@
 class Solution {
-    int func(int nums[],int n,int t,int idx){
+    int func(int nums[],int n,int t,int idx,int dp[][]){
         if(idx>=nums.length){
             if(n==t) return 1;
             return 0;
         }
-        int v1= func(nums,n+nums[idx],t,idx+1);
-        int v2= func(nums,n-nums[idx],t,idx+1);
-        return v1+v2;
+        if(dp[n+1000][idx]!=-1) return dp[n+1000][idx];
+        int v1= func(nums,n+nums[idx],t,idx+1,dp);
+        int v2= func(nums,n-nums[idx],t,idx+1,dp);
+        dp[n+1000][idx]= v1+v2;
+        return dp[n+1000][idx];
     }
     public int findTargetSumWays(int[] nums, int t) {
-        return func(nums,0,t,0);
+        
+        int max =nums[0];
+        
+        int dp[][] =new int[2001][nums.length];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        
+        return func(nums,0,t,0,dp);
     }
 }
