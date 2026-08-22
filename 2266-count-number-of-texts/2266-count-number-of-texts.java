73f4c6cfc1a102853afdd04fab3613d2b795f8b8
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-08-23 01:13:27 +0530

    Time: 534 ms (5.31%), Space: 68.5 MB (24.9%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.java
+++ 2266-count-number-of-texts/2266-count-number-of-texts.java
@@ -1,27 +1,36 @@
 class Solution {
-    int func(String s,int idx,int []dp){
-        int mod=1000000000+7;
+    int func(String s,int idx,int dp[]){
         if(idx>=s.length()) return 1;
+        int v =3;int ans=0;
+        int mod= 1000000000+7;
         if(dp[idx]!=-1) return dp[idx];
-        char ch =s.charAt(idx);
-        int l=0;long v=0;
-        if(ch=='7'||ch=='9') l= 4;
-        else l=3;
-        for(int i=idx;i<idx+l;i++){
-           if(i<s.length()&&s.charAt(i)==s.charAt(idx)) v=(v+func(s,i+1,dp))%mod;
-           else break;
+        if(s.charAt(idx)=='7'||s.charAt(idx)=='9') v=4;
+        for(int i=idx;i<idx+v;i++){
 
+            if(i<s.length()) ans= (ans+func(s,i+1,dp))%mod;
         }
-        dp[idx]= (int)v%mod;
+        dp[idx]= ans;
         return dp[idx];
-
-        
     }
     public int countTexts(String s) {
-        int dp[] =new int[s.length()];
+        long ans=1;
+       int mod= 1000000000+7;
+        char v =s.charAt(0);int l=0;
+        for(int i=0;i<s.length();i++){
+            char ch =s.charAt(i);
+            if(ch!=v) {
+                 int dp[] =new int[s.length()];
         Arrays.fill(dp,-1);
-        return func(s,0,dp);
+                ans =(ans*func(s.substring(l,i),0,dp))%mod;
+                l =i;
+                v =ch;
+            }
 
+        }
+         int dp[] =new int[s.length()];
+        Arrays.fill(dp,-1);
+        ans= (ans*func(s.substring(l,s.length()),0,dp))%mod;
+        return (int)ans;
         
     }
 }
