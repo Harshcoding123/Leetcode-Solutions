110e97ffd727e2c6f971778596a660b787948e80
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-08-22 23:23:02 +0530

    Time: 60 ms (78.28%), Space: 43.9 MB (85.63%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -2,20 +2,29 @@ class Solution {
     public int beautySum(String s) {
         int ans=0;
         for(int i=0;i<s.length();i++){
-            HashMap<Character,Integer>mp =new HashMap<>();
+           int f[]= new int [26];
             // int min =Integer.MAX_VALUE;
-            int max= Integer.MIN_VALUE;
+            
             
             for(int j= i;j<s.length();j++){
                 char ch= s.charAt(j);
-                if(!mp.containsKey(ch)) mp.put(ch,1);
-                else mp.put(ch,mp.get(ch)+1);
+                f[ch-'a']++;
+                // if(!mp.containsKey(ch)) mp.put(ch,1);
+                // else mp.put(ch,mp.get(ch)+1);
                 int min =Integer.MAX_VALUE;
+                int max= Integer.MIN_VALUE;
                 // min= Math.min(mp.get(ch),min);
-                max =Math.max(mp.get(ch),max);
-                for(var e:mp.keySet()){
-                    min= Math.min(mp.get(e),min);
+                // max =Math.max(mp.get(ch),max);
+                // for(var e:mp.keySet()){
+                //     min= Math.min(mp.get(e),min);
+                // }
+                for(int k=0;k<26;k++){
+                    if(f[k]>0){
+                        max= Math.max(max,f[k]);
+                        min= Math.min(min,f[k]);
+                    }
                 }
+
                 ans+=(max-min);
 
             }
