8b17763c8575d455965d9e7512325375fc9819e0
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-09-03 17:12:06 +0530

    Time: 1138 ms (44.23%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-select name,bonus
+select e.name,b.bonus
 from Employee as e
 left join Bonus as b
-on e.empId= b.empId
-where b.bonus<1000 or b.bonus is null
+on e.empId=b.empId
+where b.bonus<1000 or bonus is null;
+
