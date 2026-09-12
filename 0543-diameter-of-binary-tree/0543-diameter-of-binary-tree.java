b40b18d5e8653b42a5df52f73c76d56a290a54c1
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-09-12 10:40:06 +0530

    Time: 1 ms (43.29%), Space: 47 MB (73.35%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,9 +14,13 @@
  * }
  */
 class Solution {
+    int ans=0;
      int h(TreeNode root){
         if(root==null) return 0;
-        int v =Math.max(h(root.left),h(root.right));
+        int v1= h(root.left);
+        int v2 =h(root.right);
+        int v =Math.max(v1,v2);
+        ans = Math.max(ans,v1+v2);
         return v+1;
     }
     int func(TreeNode root){
@@ -26,7 +30,11 @@ class Solution {
         int v2 =func(root.right);
         return Math.max(v,Math.max(v1,v2));
     }
+    
     public int diameterOfBinaryTree(TreeNode root) {
-     return func(root);   
+    //  return func(root);  
+    int v= h(root);
+    return ans; 
+    
     }
 }
