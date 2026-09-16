8fd93f7882884529ba2c930c6f7c362839cd892f
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-09-16 16:13:07 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.91%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] a) {
+       int n =a.length;
+       if(n==0) return 0;
+       int dp[] =new int [n];
+       dp[0] =a[0];
+       if(n>1) dp[1] =Math.max(a[0],a[1]);
+       for(int i=2;i<n;i++){
+        dp[i]= Math.max(dp[i-1],dp[i-2]+a[i]);
+       }
+       return dp[n-1];
+    }
+}
