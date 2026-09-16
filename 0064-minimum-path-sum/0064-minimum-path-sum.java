5419e4357ce4a37a5f6b56b4c183d546765248ef
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-09-16 17:40:37 +0530

    Time: 4 ms (60.28%), Space: 50.1 MB (72.42%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minPathSum(int[][] g) {
+        int m= g.length;
+        int n =g[0].length;
+        int dp[][] =new int[m][n];
+        dp[0][0]= g[0][0];
+        for(int j=1;j<n;j++){
+            dp[0][j] =g[0][j]+dp[0][j-1];
+        }
+        for(int i=1;i<m;i++){
+            dp[i][0]= g[i][0]+dp[i-1][0];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]= Math.min(dp[i-1][j],dp[i][j-1])+g[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
