dd63bf7020ba0be19483bcef069044857692b966
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-09-19 10:43:56 +0530

    Time: 376 ms (13.87%), Space: 236.8 MB (23.06%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,25 @@
 class Solution {
-    int func(int nums[],int idx,int p,int dp[][]){
+    int func(int []nums,int idx,int p,int [][]dp){
         if(idx>=nums.length) return 0;
-        if(dp[idx][p]!=-1) return dp[idx][p];
-        if(p!=nums.length&&nums[idx]<=nums[p]){
-             dp[idx][p]= func(nums,idx+1,p,dp);
-             return dp[idx][p];
-        } //skip
+        if(dp[idx][p+1]!=-1) return dp[idx][p+1];
+        int skip= func(nums,idx+1,p,dp);
+        int take =0;
         
-        int v1= func(nums,idx+1,p,dp); //skip
-        
-        int v2= func(nums,idx+1,idx,dp)+1; //take
-        dp[idx][p]= Math.max(v1,v2);
-        return dp[idx][p];
+        if(p==-1||nums[idx]>nums[p]){
+            take =func(nums,idx+1,idx,dp)+1;
+            
+            dp[idx][p+1]= Math.max(skip,take);
+            return dp[idx][p+1];
+        }else{
+            dp[idx][p+1]= skip;
+            return dp[idx][p+1];
+        }
     }
     public int lengthOfLIS(int[] nums) {
-        int dp[][]= new int[nums.length][nums.length+1];
+        int dp[][]= new int [nums.length][nums.length+1];
         for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        return func(nums,0,nums.length,dp);
-        
+    return func(nums,0,-1,dp);
     }
 }
