90c5c850f8a824a8fb705fe421303ab187f5d443
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2026-09-26 13:31:18 +0530

    Time: 14 ms (59.71%), Space: 63.2 MB (37.65%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,27 +1,36 @@
 class Solution {
-    void func(int g[][],int curr,boolean vis[],HashSet<Integer>st,List<Integer>a){
-        vis[curr]= true;boolean check=true;
+    void func(int [][]g,int curr,boolean vis[],List<Integer>a,boolean path[]){
+        vis[curr] =true;
+        boolean check =true;
+        // if(g[curr].length==0) {
+        //     path[curr] =true;
+        //     check =true;
+        // }
         for(int i=0;i<g[curr].length;i++){
-            int e=g[curr][i];
-            if(!vis[e]) func(g,e,vis,st,a);
-            if(!st.contains(e)) check= false;
+            int e =g[curr][i];
+            
+            if(!vis[e]){
+                func(g,e,vis,a,path);
+            }
+            if(!path[e]) check =false;
         }
         if(check){
+            path[curr] =true;
              a.add(curr);
-             st.add(curr);
         }
+        
     }
-    
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        HashSet<Integer>st= new HashSet<>();
-        List<Integer>a= new ArrayList<>();
-        boolean vis[]= new boolean[graph.length];
-        for(int i=0;i<graph.length;i++){
-            if(!vis[i]) func(graph,i,vis,st,a);
+    public List<Integer> eventualSafeNodes(int[][] g) {
+        ArrayList<Integer>a =new ArrayList<>();
+        boolean vis[] =new boolean[g.length];
+         boolean path[] =new boolean[g.length];
+        
+        for(int i=0;i<g.length;i++){
+            if(!vis[i]){
+                func(g,i,vis,a,path);
+            }
         }
         Collections.sort(a);
-        
         return a;
-        
     }
 }
